40983bba9607e58e00ac0fa9edf942c6bebe9d21
Diana <konfederatovadianochka@yandex.ru>
2026-06-09 14:19:53 +0700

    feat: add struct Student for the binary tree node

---
+++ lab9_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+//структура узла для 1-ого задания
+struct Student {
+    std::string surname;
+    int grades[4];
+    Student* left = nullptr;
+    Student* right = nullptr;  
+};
+
+int main(){
+
+}
