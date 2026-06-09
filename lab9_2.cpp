74eb75f9b3017711dcdcd0c9a7fa8e9090c157a1
Diana <konfederatovadianochka@yandex.ru>
2026-06-09 14:46:15 +0700

    feat: add function addStudent

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -9,6 +9,48 @@ struct Student {
     Student* right = nullptr;  
 };
 
+void addStudent(Student*& head, std::string surname, int g1, int g2, int g3, int g4){
+    Student* newStudent = new Student;
+    newStudent -> surname = surname;
+    newStudent -> grades[0] = g1;
+    newStudent -> grades[1] = g2;
+    newStudent -> grades[2] = g3;
+    newStudent -> grades[3] = g4;
+    Student* curr = head;
+    
+    if(head == nullptr){
+        head = newStudent;
+        return;
+    }
+
+    while(true){
+        if(curr -> surname > newStudent -> surname){
+
+            if(curr -> left == nullptr){
+                curr -> left = newStudent;
+                return;
+            }else{
+                curr = curr -> left;
+            }
+
+        }else if(curr -> surname < newStudent -> surname){
+
+            if(curr -> right == nullptr){
+                curr -> right = newStudent;
+                return;
+            }else{
+                curr = curr -> right;
+            }
+
+        }else{
+            std::cout << "Студент с такой фамилией уже есть!\n";
+            delete newStudent;
+            return;
+        }
+    }
+}
+
 int main(){
+    Student* head = nullptr;
 
 }
