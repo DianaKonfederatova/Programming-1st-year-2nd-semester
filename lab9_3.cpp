6a0dd5287a6947020bde1aa66a9b75761524f876
Diana <konfederatovadianochka@yandex.ru>
2026-06-12 14:49:34 +0700

    feat: add fuction addStudent

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <vector>
+
+std::vector<std::list<Student>> M(17);
+int collisions[17] = {0};
 
 struct Student {
     std::string surname;
     int grades[4]; 
 };
 
-int hash_function(std::string surname){
+int hash_function(const std::string& surname){
     int sum = 0;
 
     for(char c : surname){
@@ -18,7 +23,20 @@ int hash_function(std::string surname){
     return ostatok;
 }
 
+void addStudent(const Student& stud){
+    int index = hash_function(stud.surname);
+
+    if(!table[index].empty()){
+        collisions[index]++;
+    }
+
+    table[index].push_back(stud);
+}
+
 int main(){
+    
+
+
 
 
 }
