aa81e5520662011c64243569d91c726c690ed6d1
Diana <konfederatovadianochka@yandex.ru>
2026-06-12 14:53:08 +0700

    feat: insert 12 students in list

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -3,13 +3,13 @@
 #include <list>
 #include <vector>
 
-std::vector<std::list<Student>> M(17);
-int collisions[17] = {0};
-
 struct Student {
     std::string surname;
     int grades[4]; 
 };
+
+std::vector<std::list<Student>> M(17);
+int collisions[17] = {0};
 
 int hash_function(const std::string& surname){
     int sum = 0;
@@ -34,9 +34,23 @@ void addStudent(const Student& stud){
 }
 
 int main(){
+    Student students[12] = {
+        {"Иванов", {5, 4, 5, 4}},
+        {"Котельников", {3, 3, 4, 5}},
+        {"Парышев", {5, 5, 5, 5}},
+        {"Смирнов", {4, 4, 4, 4}},
+        {"Кузнецов", {5, 3, 4, 4}},
+        {"Попов", {3, 4, 3, 3}},
+        {"Васильев", {5, 5, 4, 5}},
+        {"Фокин", {4, 5, 5, 4}},
+        {"Михайлов", {3, 3, 3, 4}},
+        {"Василькин", {5, 4, 4, 5}},
+        {"Федотов", {4, 4, 5, 5}},
+        {"Морозов", {5, 5, 5, 5}}
+    };
+
+    for (int i = 0; i < 12; i++) {
+        addStudent(students[i]);
+    }
     
-
-
-
-
 }
